8a4c728bbbf18912df098699cfac86835372ae76
Arthur Campbell <arfa.camble@gmail.com>
2021-02-15 16:51:52 +0000

    it's functional but ugly

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,41 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times { @letters << alphabet.sample }
   end
 
   def score
+    @word = params[:word]
+    @letters = params[:letters].split(' ')
+    if !word_valid_re_grid?(@word, @letters)
+      @result = "Look closer... you can't make #{@word.upcase} out of #{params[:letters]}"
+    elsif !word_valid_re_dictionary?(@word)
+      @result = "Perhaps you're not from round here, but we don't consider that sequence of letters to be pleasant"
+    else
+      @result = "You absolute spelling LEGEEEEEND #{@word} is so valid"
+    end
+  end
+
+  private
+
+  def word_valid_re_grid?(word, letters)
+    chars = word.upcase.split ''
+    chars.each do |char|
+      return false unless letters.include? char
+
+      letters.delete_at(letters.index(char))
+    end
+    true
+  end
+
+  def word_valid_re_dictionary?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    dictionary_response = open(url).read
+    dictionary_response_hash = JSON.parse(dictionary_response)
+    dictionary_response_hash['found']
   end
 end
